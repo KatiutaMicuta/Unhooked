0821832de120603b0b12a87c6a18fed8c0502ad5
Katia Holban-Radutiu <100828792+KatiutaMicuta@users.noreply.github.com>
2026-07-22 17:36:40 +0300

    add a function checking if there are more than 4 subdomains

--- src/url_analyzer.py
+++ src/url_analyzer.py
@@ -1,6 +1,6 @@
 from urllib.parse import urlparse
 
-#chechking if the url is an IP address
+#checking if the url is an IP address
 def uses_ip_address(url):
 
     host = urlparse(url).hostname
@@ -11,8 +11,19 @@ def uses_ip_address(url):
     
     return len(parts) == 4 and all(part.isdigit() for part in parts)
 
+#checking if the URL is hiding the real destination behind @
 def has_userinfo(url):
 
     netloc = urlparse(url).netloc
-    
     return "@" in netloc
+
+#if URL has more than 4 subdomains it's usually suspicious
+def has_many_subdomains(url):
+    
+    host = urlparse(url).hostname
+    if host is None:
+        return False
+    
+    parts = host.split(".")
+    return len(parts) > 4
+    
